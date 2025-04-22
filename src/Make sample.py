9c8f6efce0a7b55b26da24d1685dbc7044658fa5
HungHHong <hong.h@ufl.edu>
2025-04-22 17:58:32 -0400

    make sample ratings to be 100k

--- src/Make sample.py
+++ src/Make sample.py
@@ -6,7 +6,7 @@ output_file = "../data/ratings_sample.csv"
 
 with open(input_file, "r", encoding="utf-8") as infile, open(output_file, "w", encoding="utf-8") as outfile:
     for i, line in enumerate(infile):
-        if i > 10000:
+        if i > 100000:
             break
         outfile.write(line)
 
